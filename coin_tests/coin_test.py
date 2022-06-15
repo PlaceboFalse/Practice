ca3273b1f205ea0f3104728f09e84f758f2fb922
PlaceboFalse <57624972+PlaceboFalse@users.noreply.github.com>
2022-06-15 12:04:35 +0300

    Create coin_test.py

---
+++ coin_tests/coin_test.py
@@ -0,0 +1,14 @@
+# from unittest import TestCase, main
+# import Practice.coin_task
+import sys
+import pprint
+
+
+class coin_test():
+    def test(self):
+        # self.assertEqual(find_coin(100), [100])
+        pass
+
+
+pprint.pprint(sys.path)
+# main()
